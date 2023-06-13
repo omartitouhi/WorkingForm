d3d005b1d58aa56b182a5ffa5e0eb6b3a05541d5
Titouhi Omar <titouhiomar223@gmail.com>
2023-06-13 13:36:35 +0100

    added new css styles

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ statusTxt = form.querySelector(".button-area span");
 
 form.onsubmit = (e)=>{
     e.preventDefault(); //preventing form from submitting
+    statusTxt.style.display="block";
+
+    let xhr = new XMLHttpRequest(); //create new xml object
+    xhr.open("POST", "message.php", true); //sending post request to message.php file
+    xhr.onload = ()=>{
+
+    }
+    xhr.send();
 }
