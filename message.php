896cc5d66aabcf61fb3dd99aeecd0ce4491ee6e0
Titouhi Omar <titouhiomar223@gmail.com>
2023-06-13 14:11:54 +0100

    final comitement contact page completed

--- message.php
+++ message.php
@@ -1,1 +1,26 @@
 <?php
+    //get all form values
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $website = $_POST['website'];
+    $message = $_POST['message'];
+
+    if(!empty($email) && !empty($message)){//if email and message fiels are not empty
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $receiver ="titouhiomar1@gmail.com";
+            $subject = "Form: $name <$email>"; //subject of the email
+            $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message\n\nRegards, \n$name";
+            $sender = "From: $email";//sender email
+            if(mail($receiver,$subject,$body,$sender)){
+                echo "your message has been send";
+            }else{
+                echo "Sorry, Failed to send yoour message";
+            }
+        }else{
+            echo "Enter a valid email address";
+        }
+    }else{
+        echo "Email and Message field are required!";
+    }
+?>
--- script.js
+++ script.js
@@ -3,12 +3,25 @@ statusTxt = form.querySelector(".button-area span");
 
 form.onsubmit = (e)=>{
     e.preventDefault(); //preventing form from submitting
+    statusTxt.style.color="#0D6EFD";
     statusTxt.style.display="block";
 
     let xhr = new XMLHttpRequest(); //create new xml object
     xhr.open("POST", "message.php", true); //sending post request to message.php file
-    xhr.onload = ()=>{
-
+    xhr.onload = ()=>{//once ajax loaded
+        if(xhr.readyState === 4 && xhr.status ===200){//if aja// x response status is 200 & ready tatus is 4 means there is no any error
+            let response = xhr.response; //storing ajax response in a response variable
+            if (response.indexOf("Email and Message field are required!")!==-1 ||response.indexOf("Enter a valid email address")||response.indexOf("Sorry, Failed to send yoour message")){
+                statusTxt.style.color="red";
+            }else{
+                form.reset();
+                setTimeout(()=>{
+                    statusTxt.style.display = "none";
+                },3000);
+            }
+            console.log(response);
+            statusTxt.innerText = response;        }
     }
-    xhr.send();
+    let formData = new FormData(form); //creating new FormData obj. this obj is used to send formdata
+    xhr.send(formData); //sending form data
 }
